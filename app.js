a7e5bb54abe7cab663f0b593a0ec21a88726e1b9
hossam-khalaf <hossam.khalaf@med.au.edu.eg>
2020-08-01 17:21:22 +0200

    general basic Dom minuplation with basic style

---
+++ app.js
@@ -0,0 +1,46 @@
+const users = [
+  {
+    name: 'Helen Coppola',
+    avatar: 'https://www.w3schools.com/w3images/avatar5.png',
+  },
+  {
+    name: 'Nick Ronins',
+    avatar: 'https://www.w3schools.com/bootstrap4/img_avatar1.png',
+  },
+  {
+    name: 'Chris Harris',
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+  },
+  {
+    name: 'Sandra Osborne',
+    avatar: 'https://www.w3schools.com/howto/img_avatar2.png',
+  },
+  {
+    name: 'Tim Johnson',
+    avatar: 'https://www.w3schools.com/bootstrap4/img_avatar1.png',
+  },
+  {
+    name: 'Antonette Martinez',
+    avatar: 'https://www.w3schools.com/w3images/avatar5.png',
+  },
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+  const appElem = document.getElementById('app');
+  const listUserCards = document.createElement('div');
+
+  users.forEach((user) => {
+    const userCard = document.createElement('div');
+    userCard.className = 'user-card';
+    userCard.innerText = user.name;
+
+    const userAvatar = document.createElement('img');
+    userAvatar.className = 'user-avatar';
+    userAvatar.src = user.avatar;
+
+    userCard.prepend(userAvatar);
+    listUserCards.append(userCard);
+  });
+
+  appElem.append(listUserCards);
+});
